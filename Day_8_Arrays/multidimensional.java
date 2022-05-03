5992195316ff68a33d0e7784cdac8143db761cfb
Preeti Chaudhari <preetischaudhari1201@gmail.com>
2022-05-03 14:09:04 +0530

    arrays

---
+++ Day_8_Arrays/multidimensional.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class multidimensional {
+    public static void main(String[] args) {
+
+        int[][]  arr = new int[3][]; //[rows][columns] //no.of rows is mandatory
+
+        int[][] array = {
+                {1,2,3},
+                {4,5},
+                {7,8,9,9}
+        };
+    }
+}
---
+++ Day_8_Arrays/passing_inFunctions.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class passing_inFunctions {
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,4,5};
+        System.out.println(Arrays.toString(nums));
+        change(nums);
+        System.out.println(Arrays.toString(nums));
+    }
+
+     static void change(int[] arr)
+     {
+        arr[0] = 99;
+     }
+}
